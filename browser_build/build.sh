1328eba1e39d9e769b463864eb9dfc7213bc7445
Johannes Bader <jogus@live.de>
2017-10-17 23:59:45 -0700

    \r\n

--- browser_build/build.sh
+++ browser_build/build.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-# check for emcc
+# check for emcc (tested with 1.37.22)
 command -v emcc || echo ""
 command -v emcc || echo "!!!"
 command -v emcc || echo "'emcc' not found, please make sure it's on the \$PATH"
@@ -15,9 +15,9 @@ command -v emcc || exit 1
 # copy stuff
 mkdir -p ../www/user-data-path
 cp index.html ../www
-cp config.init ../www/user-data-path
+cp config.ini ../www/user-data-path
 
 # build
 set -e
-bash ./build_compile.sh
-bash ./build_link.sh
+sed -i 's/\r$//' ./build_compile.sh && ./build_compile.sh
+sed -i 's/\r$//' ./build_link.sh    && ./build_link.sh
